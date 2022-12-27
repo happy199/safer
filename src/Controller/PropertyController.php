b66a4b3f37fbfe803724f58888c8f5c90ffa0218
Happy Tidjani <tidjani.happy@gmail.com>
2022-12-27 20:40:40 +0100

    Close view system

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 
 #[Route('/properties', name: 'property_')]
 class PropertyController extends AbstractController
@@ -17,7 +18,7 @@ class PropertyController extends AbstractController
     {
         $this->entityManager = $entityManager;
     }
-    
+
     #[Route('/', name: 'index')]
     public function index(): Response
     {
@@ -28,9 +29,14 @@ class PropertyController extends AbstractController
     }
 
     #[Route('/{slug}', name: 'details')]
-    public function details(Property $property): Response
+    public function details(Property $property, ManagerRegistry $doctrine): Response
     {
-        // dd($property->getTitle());
+        $thisproperty = $this->entityManager->getRepository(Property::class)->findOneBy(['slug' => $property->getSlug()]);;
+        $entityManager = $doctrine->getManager();
+        $views = $thisproperty->getNbview();
+        $moreviews = $views + 1;
+        $thisproperty->setNbview($moreviews);
+        $entityManager->flush();
         return $this->render('property/details.html.twig', [
             'property' => $property
         ]);
